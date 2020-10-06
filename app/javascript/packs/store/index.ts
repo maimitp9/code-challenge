cf006a4eaf77cf6db05ba487a2ac03b492e472c5
Maimit Patel <maimit.patel@unifa-e.com>
2020-10-06 12:12:18 +0900

    Add Axios, ApiClient

--- app/javascript/packs/store/index.ts
+++ app/javascript/packs/store/index.ts
@@ -1,20 +1,28 @@
-import Vue from 'vue';
-import Vuex, { StoreOptions, Store } from 'vuex';
-import { RootState } from './types';
+import Vue from "vue";
+import Vuex, { StoreOptions } from "vuex";
+import { RootState } from "./types";
+
+import ApiClient from "../lib/ApiClient";
+import axios from "./axios";
 
 Vue.use(Vuex);
 
+const apiClient = new ApiClient({
+  axios: axios,
+  version: 0,
+});
+
 const initialState: RootState = {
-  clientVersion: 0
+  apiClient: apiClient,
+  clientVersion: 0,
 };
 
-
-import { mutations } from './mutations';
+import { mutations } from "./mutations";
 
 const store: StoreOptions<RootState> = {
   state: initialState,
   mutations,
-  modules: {}
+  modules: {},
 };
 
 export default new Vuex.Store<RootState>(store);
